b0fcc7500d8a54457670616d694c388b356ddb2b
Alibek <alibek.mem@mail.ru>
2020-08-19 20:51:33 +0600

    API keys
    API keys hidden in .env file

--- Wanderlust/public/main.js
+++ Wanderlust/public/main.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 // Foursquare API Info
-const clientId = 'INBBBTB3TL2T1QZNGEZRVVYWVZWSB1A21IFJYODCNLIS1DO1';
-const clientSecret = 'UT3YJQOF2YWMZORUBAYZ5KCXQVN5U3ZD3BN2LSUMHTRJWVYD';
+const clientId = process.env.FORECAST_CLIENT_ID;
+const clientSecret = process.env.FORECAST_CLIENT_SECRET;
 const url = 'https://api.foursquare.com/v2/venues/explore?near=';
-
 // OpenWeather Info
-const openWeatherKey = '1585ab7611be7077c38574f533eab717';
+const openWeatherKey = process.env.OPEN_WEATHER_API_KEY;
 const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 // Page Elements
